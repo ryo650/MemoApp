64ea3d643e1281a9e7ee1ef192905f2b03248866
sasakiryo <ryo.s34@icloud.com>
2026-03-18 10:13:03 +0900

    add KeyboardAvoidingView.jsx

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -1,27 +1,46 @@
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
 
-import { router } from 'expo-router';
+import { router, useLocalSearchParams} from 'expo-router';
+
+import { onSnapshot, doc } from 'firebase/firestore';
+import { useState,useEffect } from 'react';
 
 import CircleButton from '../../components/CircleButton';
 import Icon from '../../components/Icon';
+import { auth, db } from '../../config';
+import { type Memo } from '../../../types/memo';
 
 const handlePress = (): void => {
     router.push('/memo/edit')
 }
 
 const Detail = () => {
+    const {id} = useLocalSearchParams()
+    console.log(id)
+    const [memo, setMemo] = useState<Memo | null>(null)
+    useEffect(() => {
+        if (auth.currentUser === null)  {return}
+        const ref = doc(db, `users/${auth.currentUser.uid}/memos`,String(id))
+        const unsbscribe = onSnapshot(ref, (memoDoc) => {
+            const { bodyText, updatedAt } = memoDoc.data() as Memo
+            setMemo({
+                id: memoDoc.id,
+                bodyText,
+                updatedAt
+            })
+        })
+        return unsbscribe
+    },[])
     return (
         <View style={styles.conttainer}>
             <View style={styles.memoHeader}>
-                <Text style={styles.memoTitle}>買い物リスト</Text>
-                <Text style={styles.memoDate}>2023年10月1日 10:00</Text>
+                <Text numberOfLines={1} style={styles.memoTitle}>{memo?.bodyText}</Text>
+                <Text style={styles.memoDate}>{memo?.updatedAt?.toDate().toLocaleString('ja-JP')}</Text>
             </View>
 
             <ScrollView style={styles.memoBody}>
                 <Text style={styles.memoBodyText}>
-                    買い物リスト
-                    書体やレイアウトなどを確認するために用います。
-                    本文用なので使い方を間違えると不自然に見えることもありますので要注意。
+                    {memo?.bodyText}
                 </Text>
             </ScrollView>
 
@@ -57,10 +76,10 @@ const styles = StyleSheet.create({
          lineHeight: 16
     },
     memoBody: {
-        paddingVertical: 32,
         paddingHorizontal:27
     },
     memoBodyText: {
+        paddingVertical: 32,
         fontSize: 16,
         lineHeight: 24,
         color: '#000000'
--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,16 +1,29 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
-import { Link} from 'expo-router';
+import { Link } from 'expo-router';
 
 import Icon from './Icon'
 
-const memoListItem = () => {
+import { type Memo } from '../../types/memo'
+
+interface Props {
+    memo: Memo
+}
+
+const memoListItem = (props: Props) => {
+    const { memo } = props
+    const { bodyText, updatedAt } = memo
+    if (bodyText === null || updatedAt === null) { return null }
+    const dateString = updatedAt.toDate().toLocaleString('ja-JP')
     return (
-        <Link href={'/memo/detail'} asChild>
+        <Link
+            href={{ pathname: 'memo/detail', params: { id: memo.id}}}
+            asChild
+        >
             <TouchableOpacity style={styles.memoListItem}>
                 <View>
-                    <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                    <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
+                    <Text numberOfLines={1} style={styles.memoListItemTitle}>{bodyText}</Text>
+                    <Text style={styles.memoListItemDate}>{dateString}</Text>
                 </View>
                 <TouchableOpacity>
                     <Icon name='delete' size={32} color='#B0B0B0' />
